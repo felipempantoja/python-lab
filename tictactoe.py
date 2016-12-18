25f3a8d7f16431b5b2628e6b3ab18fbcac374fc3
Felipe Pantoja <felipempantoja@gmail.com>
2016-12-18 10:56:10 -0200

    OOP improvements

--- tictactoe.py
+++ tictactoe.py
@@ -1,61 +1,79 @@
-from subprocess import call
+class TicTacToe:
+    def __init__(self):
+        self.board = range(9)
+        self.player = {1: 'x', 2: 'o'}
+        self.current_player = 2
+        self.winning_sequences = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-board = range(9)
-player = {1: 'X', 2: 'O'}
-current_player = 2
-winner_sequences = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-
-def draw_board():
-    print '''
-    _{}_|_{}_|_{}_
-    _{}_|_{}_|_{}_
-     {} | {} | {}
-    '''.format(board[0],board[1],board[2],board[3],board[4],board[5],board[6],board[7],board[8])
-
-def get_remaining_positions():
-    return map(str, [ e for e in board if type(e) is int ])
-
-def fill_board(position, player_assign):
-    board[position] = player_assign
-
-def is_endgame(current_player):
-    return is_board_fullfilled() or is_winner(current_player)
-
-def is_board_fullfilled():
-    return all(type(e) is str for e in board)
-
-def is_winner(current_player):
-    signal = player[current_player]
-    for sequence in winner_sequences:
-        board_seq = [ e for i, e in enumerate(board) if i in sequence ]
-        if all(e == signal for e in board_seq):
-            return True
-    return False
+    def draw_board(self):
+        print '''
+        _{}_|_{}_|_{}_
+        _{}_|_{}_|_{}_
+         {} | {} | {}
+        '''.format(
+            self.board[0],
+            self.board[1],
+            self.board[2],
+            self.board[3],
+            self.board[4],
+            self.board[5],
+            self.board[6],
+            self.board[7],
+            self.board[8]
+        )
     
-def show_results():
-    print '\n\nResults'
-    if is_winner(current_player):
-        print 'Winner: Player {}'.format(current_player)
-    elif is_board_fullfilled():
-        print 'Draw!'
+    def toggle_player(self):
+        self.current_player = 2 if self.current_player == 1 else 1
+        return self.player[self.current_player]
+
+    def get_remaining_positions(self):
+        return map(str, [ e for e in self.board if type(e) is int ])
+
+    def fill_board(self, position, player_assign):
+        self.board[position] = player_assign
+
+    def is_running(self):
+        return not game.is_board_fullfilled() and not game.is_current_player_winner()
+
+    def is_board_fullfilled(self):
+        return all(type(e) is str for e in self.board)
+
+    def is_current_player_winner(self):
+        signal = self.player[self.current_player]
+        for sequence in self.winning_sequences:
+            board_seq = [ e for i, e in enumerate(self.board) if i in sequence ]
+            if all(e == signal for e in board_seq):
+                return True
+        return False
+        
+    def show_results(self):
+        print '\n\nResults'
+        if self.is_current_player_winner():
+            print 'Winner: Player {}'.format(self.current_player)
+        elif self.is_board_fullfilled():
+            print 'Draw!' 
 
 
 print 'Welcome to Tic Tac Toe in Python'
 
-while not is_endgame(current_player):
-    current_player = 2 if current_player == 1 else 1
-    # call(['clear'])
-    draw_board()
-    remaining_positions = get_remaining_positions()
+game = TicTacToe()
+
+while game.is_running():
+    player = game.toggle_player()
+    game.draw_board()
+
+    remaining_positions = game.get_remaining_positions()
     print 'Remaining positions: {}'.format(', '.join(remaining_positions))
+
     try:
-        position = int(raw_input('Player {}: '.format(current_player)))
+        position = int(raw_input('Player {}: '.format(game.current_player)))
         if str(position) not in remaining_positions:
             print 'Please, choose one of the remaining positions'
             continue
     except ValueError:
         print 'Invalid number, try again'
         continue
-    fill_board(position, player[current_player])
+
+    game.fill_board(position, player)
 else:
-    show_results()
+    game.show_results()
