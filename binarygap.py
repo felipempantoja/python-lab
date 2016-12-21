ec69e7ea2f97f52a0e8ec287f4d8912095e489a1
Felipe Pantoja <felipempantoja@gmail.com>
2016-12-21 10:28:49 -0200

    binary gap algorithm - 2 resolutions

---
+++ binarygap.py
@@ -0,0 +1,38 @@
+#This program finds the biggest sequence of zeroes, ONLY those surrounded by ones
+
+import os
+
+decimal = 0
+
+while type(decimal) is int:
+    try:
+        decimal = int(raw_input('Type a decimal number (or any non-number to quit): '))
+        
+        os.system('clear')
+
+        first_one_found = False
+
+        sequence = 0
+        biggest = 0
+
+        while decimal > 0:
+            remainder = decimal % 2
+
+            if not first_one_found and remainder == 0:
+                decimal //= 2
+            elif remainder == 1:
+                first_one_found = True
+                sequence = 0
+                decimal //= 2
+            else:
+                sequence += 1
+                decimal //= 2
+
+            biggest = max(sequence, biggest)
+
+        print '''
+        Decimal number: {}
+        The biggest zeroes sequence surrounded by ones is: {}
+        '''.format(decimal, biggest)
+    except ValueError:
+        break
---
+++ binarygap_pythonic.py
@@ -0,0 +1,23 @@
+#This program finds the biggest sequence of zeroes, ONLY those surrounded by ones
+
+import os
+
+decimal = 0
+
+while type(decimal) is int:
+    try:
+        decimal = int(raw_input('Type a decimal number (or any non-number to quit): '))
+        
+        os.system('clear')
+        
+        binary = bin(decimal)[2:]
+        binary_sequences = binary.rstrip('0').split('1')
+        biggest_zeroes_sequence = len(max(binary_sequences))
+
+        print '''
+        Decimal number: {}
+        Binary number: {}
+        The biggest zeroes sequence surrounded by ones is: {}
+        '''.format(decimal, binary, biggest_zeroes_sequence)
+    except ValueError:
+        break
