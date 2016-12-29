0aaae33f04ff85d5b4c6420faa1666942357ecbc
Felipe Pantoja <felipempantoja@gmail.com>
2016-12-29 13:36:52 -0200

    perm check algorithm

--- permcheck.py
+++ permcheck.py
@@ -1,12 +1,11 @@
-#question: https://codility.com/demo/results/training5XVT4Y-DSX/
+#question: https://codility.com/programmers/lessons/4-counting_elements/perm_check/
 #my solution: https://codility.com/demo/results/trainingAWFGY9-3XD/
 
 def solution(A):
     a = set(A)
     len_elements = len(a)
     if len(A) != len_elements: return 0
-    sum_numbers = sum(a)
     sum_elements = len_elements * (len_elements+1) / 2
-    return 1 if sum_numbers == sum_elements else 0
+    return 1 if sum(a) == sum_elements else 0
 
 print solution(map(int, raw_input().split()))
